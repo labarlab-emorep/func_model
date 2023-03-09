ac87b86963317a81e1f856a0d5b565f241f3987b
Nathan Muncy <nathan.muncy@duke.edu>
2023-03-09 11:06:10 -0500

    [VER] added versioning

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
+exec(open("func_model/_version.py").read())
+
 setup(
     name="func_model",
-    version="0.2",
+    version=__version__,  # noqa: F821
     packages=find_packages(),
     entry_points={
         "console_scripts": [
