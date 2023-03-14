754c1bcd47d22bd2e8c61dfa5ce7f958f0f0a27d
Nathan Muncy <nathan.muncy@duke.edu>
2023-03-14 15:31:13 -0400

    [VER] ready for release

--- func_model/entrypoint.py
+++ func_model/entrypoint.py
@@ -15,12 +15,13 @@ def main():
         afni_extract : Extract emotion beta values from deconvolve AFNI as a matrix
         afni_univ    : Conduct univariate tests in AFNI
         afni_mvm     : Conduct multivariate tests in AFNI
+        fsl_model    : Conduct FSL-style first-level regressions
+        fsl_extract  : Extract emotion beta values from FSL first-level as a matrix
+        fsl_map      : Make binary masks from classifier output
 
     Sub-packages under development:
 
-        fsl_model    : Conduct FSL-style first- and second-level regressions
-        fsl_extract  : Extract emotion beta values from FSL first-level as a matrix
-        fsl_map      : Make binary masks from classifier output
+        fsl_model    : Plan to add second-level and resting-state models
 
     The sub-packages afni_model, fsl_model are written specifically for the
     Duke Compute Cluster (DCC), while afni_extract, afni_univ, afni_mvm,
