3536c8a4ce2b18e55361ef7b1f8f2c7bbb728827
Nathan Muncy <nathan.muncy@duke.edu>
2024-08-06 10:02:32 -0400

    [DEV] added cluster table generation

--- bin/lmer_montages.sh
+++ bin/lmer_montages.sh
@@ -2,19 +2,27 @@
 
 function Usage {
     cat <<USAGE
-    Usage: $0 -s <stat-name> [-g group-path] [-t template]
+    Usage: $0 -s <stat-name> [-c int] [-g group-path] [-n int] [-t template]
 
-    Generate montages for emotion main effects of afni_lmer output. PNG files
-    are written to:
-        <group-path>/<stat-name>/subbrick_montages
+    Generate montages and cluster tables for emotion main effects of
+    afni_lmer output.
+
+    PNG montage files are written to
+        <group-path>/<stat-name>/emotion_montages
+    and TXT tables are written to
+        <group-path>/<stat-name>/emotion_tables
 
     Requires:
         - Template file to exist in <group-path>
         - Output of func_model's afni_lmer
 
     Optional Arguments:
+        -c <int> = Clusterize threshold from 3dClustSim for bisided p=0.001
+            (default: 12)
         -g <path> = Location of group directory
             (default: /mnt/keoki/experiments2/EmoRep/Exp2_Compute_Emotion/analyses/model_afni_group)
+        -n [1|2|3] = Nearest neighbors
+            (default: 2)
         -t <str> = Template name
             (default: tpl-MNI152NLin6Asym_res-01_desc-brain_T1w.nii.gz)
 
@@ -30,12 +38,20 @@ USAGE
 # Set default variables
 group_dir=/mnt/keoki/experiments2/EmoRep/Exp2_Compute_Emotion/analyses/model_afni_group
 tpl_file=tpl-MNI152NLin6Asym_res-01_desc-brain_T1w.nii.gz
+n_nbr=2
+c_thr=12
 
 # Capture arguments
-while getopts ":g:s:t:h" OPT; do
+while getopts ":c:g:n:s:t:h" OPT; do
     case $OPT in
+    c)
+        c_thr=${OPTARG}
+        ;;
     g)
         group_dir=${OPTARG}
+        ;;
+    n)
+        n_nbr=${OPTARG}
         ;;
     s)
         stat_name=${OPTARG}
@@ -98,7 +114,11 @@ if [ ! -f ${stat_path}.HEAD ]; then
 fi
 
 # Set subbrick emotion name, coefficient, and stat IDs
-sub_name=(Amu Ang Anx Awe Cal Cra Dis Exc Fea Hor Joy Neu Rom Sad Sur)
+sub_name=(
+    Amusement Anger Anxiety Awe Calmness
+    Craving Disgust Excitement Fear Horror
+    Joy Neutral Romance Sadness Surprise
+)
 sub_coef=(3 9 15 21 27 33 39 45 51 57 63 69 75 81 87)
 sub_stat=(4 10 16 22 28 34 40 46 52 58 64 70 76 82 88)
 
@@ -108,14 +128,16 @@ if [ ${#sub_name[@]} != ${#sub_coef[@]} ] || [ ${#sub_name[@]} != ${#sub_stat[@]
     exit 1
 fi
 
-# Setup output directory
-out_dir=$(dirname $stat_path)/subbrick_montages
-mkdir -p $out_dir
+# Setup output directories
+mnt_dir=$(dirname $stat_path)/emotion_montages
+tbl_dir=$(dirname $stat_path)/emotion_tables
+mkdir -p $mnt_dir $tbl_dir
 
-# Make montage for each emotion
 c=0
 while [ $c -lt ${#sub_name[@]} ]; do
-    echo -e "\n\nDrawing ${sub_name[$c]}_mont files to:\n\t${out_dir}\n\n" >&1
+
+    # Make montage for each emotion
+    echo -e "\n\nDrawing ${sub_name[$c]} montages to:\n\t${mnt_dir}\n\n" >&1
     @chauffeur_afni \
         -ulay $tpl_path \
         -olay $stat_path \
@@ -126,7 +148,7 @@ while [ $c -lt ${#sub_name[@]} ]; do
         -thr_olay_pside bisided \
         -set_subbricks -1 ${sub_coef[$c]} ${sub_stat[$c]} \
         -opacity 6 \
-        -prefix ${out_dir}/${sub_name[$c]}_mont \
+        -prefix ${mnt_dir}/${sub_name[$c]} \
         -montx 5 \
         -monty 5 \
         -set_xhairs OFF \
@@ -137,6 +159,20 @@ while [ $c -lt ${#sub_name[@]} ]; do
         -label_size 2 \
         -box_focus_slices AMASK_FOCUS_ULAY \
         -do_clean
+
+    # Make a cluster table for each emotion
+    echo -e "\n\nWriting ${sub_name[$c]} tables to:\n\t${tbl_dir}\n\n" >&1
+    3dClusterize \
+        -nosum \
+        -1Dformat \
+        -inset $stat_path \
+        -idat ${sub_coef[$c]} \
+        -ithr ${sub_stat[$c]} \
+        -orient LPI \
+        -NN $n_nbr \
+        -clust_nvox $c_thr \
+        -bisided p=0.001 \
+        >${tbl_dir}/${sub_name[$c]}_table.txt
 
     let c+=1
 done
